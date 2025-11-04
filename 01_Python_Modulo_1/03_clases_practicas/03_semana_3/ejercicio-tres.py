d36c66b5e1dab5c60d716016b2d06c3746444097
luisguillermomolero <luisguillermomolero@gmail.com>
2025-11-03 21:30:34 -0500

    commit

---
+++ 01_Python_Modulo_1/03_clases_practicas/03_semana_3/ejercicio-tres.py
@@ -0,0 +1,4 @@
+# Desarrolle una app en python que permita sumar los números de 1 al 100
+# Desarrolle una app en python que permita calcular el cuadrado de los números de 1 al 5
+# Desarrolle una app en python que permita mostrar solo las vocales de una palabra
+# Desarrolle una app en python que permita imprimir la tabla de multiplicar de un número ingresado por teclado
