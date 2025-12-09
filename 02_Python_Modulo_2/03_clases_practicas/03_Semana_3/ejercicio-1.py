f68b99c7c754eb9f9b7f8c281fb851323e510c45
luisguillermomolero <luisguillermomolero@gmail.com>
2025-12-09 09:07:13 -0500

    commit

--- 02_Python_Modulo_2/03_clases_practicas/03_Semana_3/ejercicio-1.py
+++ 02_Python_Modulo_2/03_clases_practicas/03_Semana_3/ejercicio-1.py
@@ -48,6 +48,67 @@ class Chef(Empleado):
 
 # Composición
 class Cocina:
+    def __init__(self, chefs: List[Chef]) -> None:
+        self.chefs = chefs
     
+    @property
+    def chefs(self) -> List[Chef]:
+        return self._chefs
+    
+    @chefs.setter
+    def chefs(self, lista_chefs: List[Chef]) -> None:
+        if isinstance(lista_chefs, list) and all(isinstance(c, Chef) for c in lista_chefs):
+            self._chefs = lista_chefs
+        else:
+            raise ValueError("Debe proporcionar una lista valida de objetos de tipo Chef.")
+    
+    def operar(self) -> None:
+        for chef in self.chefs:
+            chef.trabajar()
 
+#Agregación/Composición
+class Restaurante:
+    def __init__(self, nombre: str) -> None:
+        self.nombre = nombre
+        self.clientes: List[Cliente] = [] # Agregación
+        self.cocina = Cocina([Chef("Luis"), Chef("Daniel")]) # Composición muy fuerte
     
+    @property
+    def nombre(self) -> str:
+        return self._nombre
+    
+    @nombre.setter
+    def nombre(self, nuevo_nombre: str) -> None:
+        if isinstance(nuevo_nombre, str) and nuevo_nombre.strip():
+            self._nombre = nuevo_nombre
+        else:
+            raise ValueError("El nombre del Restaurante debe ser un nombre válido")
+    
+    def agregar_cliente(self, cliente: Cliente) -> None:
+        if isinstance(cliente, Cliente):
+            self.clientes.append(cliente)
+        else:
+            raise ValueError("Solo se pueden agregar elementos de tipo cliente.")
+        
+    def iniciar_servicio(self) -> None:
+        print(f"\n*** El Restaurante {self.nombre} iniciando el servicio ***")
+        print("\nAtendiendo a los siguientes clientes:")
+        
+        for cliente in self.clientes:
+            cliente.presentar()
+        
+        print("\nCocina en operación")
+        self.cocina.operar()
+
+def main() -> None:
+    restaurante = Restaurante("El Buen Gusto")
+    
+    restaurante.agregar_cliente(Cliente("Daniel Vélez"))
+    restaurante.agregar_cliente(Cliente("Oscar Rodriguez"))
+    
+    restaurante.iniciar_servicio()
+
+if __name__ == "__main__":
+    main()
+
+            
---
+++ 02_Python_Modulo_2/03_clases_practicas/04_Semana_4/proyecto_clientes/models/rol.py
@@ -0,0 +1,36 @@
+from abc import ABC, abstractmethod
+
+class Rol(ABC):
+    
+    @abstractmethod
+    def aplicar_regla(self, monto: float) -> float:
+        pass
+    
+    @abstractmethod
+    def descripcion(self) -> str:
+        pass
+    
+class RolRegular(Rol):
+    
+    def aplicar_regla(self, monto: float) -> float:
+        return monto
+    
+    def descripcion(self) -> str:
+        return "Cliente Regular"
+
+class RolVIP(Rol):
+    
+    def aplicar_regla(self, monto: float) -> float:
+        return round(monto * 0.90, 2)
+    
+    def descripcion(self) -> str:
+        return "Cliente VIP"
+
+class RolEmpresarial(Rol):
+    
+    def aplicar_regla(self, monto: float) -> float:
+        return monto
+    
+    def descripcion(self) -> str:
+        return "Cliente Empresarial"
+    
