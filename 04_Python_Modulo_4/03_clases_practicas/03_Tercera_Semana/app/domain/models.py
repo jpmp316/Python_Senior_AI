0dd8e5d757c135e07cbd4f77d0c4754138c4f3ae
luisguillermomolero <luisguillermomolero@gmail.com>
2026-03-04 21:47:49 -0500

    commit

---
+++ 04_Python_Modulo_4/03_clases_practicas/03_Tercera_Semana/app/domain/models.py
@@ -0,0 +1,10 @@
+from domain.exceptions import EdadNegativaError
+
+class Usuario:
+    def __init__(self, nombre: str, edad: int) -> None:
+        if edad <= 0:
+            raise EdadNegativaError("La edad no puede ser cero o menor a cero")
+        
+        self.nombre = nombre
+        self.edad = edad
+
