79907790a4998dcf28d5cf4bf57fadd615a5a4e3
luisguillermomolero <luisguillermomolero@gmail.com>
2026-03-09 20:24:22 -0500

    commit

---
+++ 04_Python_Modulo_4/03_clases_practicas/03_Tercera_Semana/app/application/user_service.py
@@ -0,0 +1,9 @@
+from infrastructure.file_repository import FileRepository
+
+class UserService:
+    def __init__(self, repository: FileRepository):
+        self.repository = repository
+    
+    def procesar(self):
+        return self.repository.read_users()
+
